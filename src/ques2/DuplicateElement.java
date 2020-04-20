ae2a2924054a8b7b5592337bc87ba1f8b8325a14
Aditi Agrawal <aditi.agrawal_cs18gla.ac.in>
2020-04-20 21:42:56 +0530

    completed

--- src/ques2/DuplicateElement.java
+++ src/ques2/DuplicateElement.java
@@ -1,4 +1,30 @@
 package ques2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DuplicateElement {
+    public void duplicateElement(int arr[]) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        boolean duplicate = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                count = map.get(arr[i]);
+                map.put(arr[i], count + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.print(entry.getKey() + " ");
+                duplicate = true;
+            }
+        }
+        if (!duplicate) {
+            System.out.println("-1");
+        }
+    }
 }
--- src/ques2/MyMain2.java
+++ src/ques2/MyMain2.java
@@ -1,4 +1,17 @@
 package ques2;
+import java.util.Scanner;
 
 public class MyMain2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter size of array...");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter elements...");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        DuplicateElement obj = new DuplicateElement();
+        obj.duplicateElement(arr);
+    }
 }
